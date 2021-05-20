3727fa3d0a58c558e4653ede91c78b7eabe51afd
DarkforcesII <35416278+DarkforcesII@users.noreply.github.com>
2021-05-20 11:52:46 -0700

    added code calls from main activity class

--- app/src/main/java/com/example/googledatavasetest/MainActivity.java
+++ app/src/main/java/com/example/googledatavasetest/MainActivity.java
@@ -1,14 +1,108 @@
 package com.example.googledatavasetest;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.googledatavasetest.R;
+import com.example.googledatavasetest.UserModal;
+import com.example.googledatavasetest.UserRVAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    // String URL
+    String sheetURL = "https://spreadsheets.google.com/feeds/list/1mf9HXeJLNFDaBXYgBrdqDBIWj" +
+            "noNRf_K1WEr_wYSZso/od6/public/values?alt=json";
+
+    // creating a variable for our array list, adapter class,
+    // recycler view, progressbar, nested scroll view
+    private ArrayList<UserModal> userModalArrayList;
+    private UserRVAdapter userRVAdapter;
+    private RecyclerView userRV;
+    private ProgressBar loadingPB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // creating a new array list.
+        userModalArrayList = new ArrayList<>();
+
+        // initializing our views.
+        userRV = findViewById(R.id.idRVUsers);
+        loadingPB = findViewById(R.id.idPBLoading);
+
+        // calling a method to load our API.
+        getDataFromAPI();
+    }
+
+    private void getDataFromAPI() {
+
+        // creating a string variable for URL.
+        String url = sheetURL;
+
+        // creating a new variable for our request queue
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+
+        // creating a variable for our JSON object request and passing our URL to it.
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                loadingPB.setVisibility(View.GONE);
+                try {
+                    JSONObject feedObj = response.getJSONObject("feed");
+                    JSONArray entryArray = feedObj.getJSONArray("entry");
+                    for(int i=0; i<entryArray.length(); i++){
+                        JSONObject entryObj = entryArray.getJSONObject(i);
+                        String firstName = entryObj.getJSONObject("gsx$firstname").getString("$t");
+                        String lastName = entryObj.getJSONObject("gsx$lastname").getString("$t");
+                        String email = entryObj.getJSONObject("gsx$email").getString("$t");
+                        String avatar = entryObj.getJSONObject("gsx$avatar").getString("$t");
+                        userModalArrayList.add(new UserModal(firstName, lastName, email, avatar));
+
+                        // passing array list to our adapter class.
+                        userRVAdapter = new UserRVAdapter(userModalArrayList, MainActivity.this);
+
+                        // setting layout manager to our recycler view.
+                        userRV.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+
+                        // setting adapter to our recycler view.
+                        userRV.setAdapter(userRVAdapter);
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // handline on error listner method.
+                Toast.makeText(MainActivity.this, "Fail to get data..", Toast.LENGTH_SHORT).show();
+            }
+        });
+        // calling a request queue method
+        // and passing our json object
+        queue.add(jsonObjectRequest);
     }
 }
